dbe976aca2fbfeb600bcd478accf161b1ac66dcb
mactul <mactulgames@gmail.com>
2024-03-04 18:40:00 +0100

    fix the fuzzer to have random picked flags

--- fuzzer.c
+++ fuzzer.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "src/readpe.h"
+
+uint16_t generate_random_flags_and_reset_seed()
+{
+    uint16_t r;
+    struct timespec t;
+
+    clock_gettime(CLOCK_REALTIME, &t);
+
+    srand(t.tv_nsec);
+    r = rand();
+    srand(1);
+
+    return r;
+}
 
 extern int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
@@ -13,11 +28,11 @@ extern int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
     fclose(test_file);
 
 
-    srand(1);
+    uint16_t flags = (uint16_t)generate_random_flags_and_reset_seed();
 
-    printf("start parsing a file of size %lu\n", Size);
+    printf("start parsing a file of size %lu with flags %d\n", Size, flags);
 
-    PE_Information* pe_information = read_pe("./fuzzed.exe", (uint16_t)rand());
+    PE_Information* pe_information = read_pe("./fuzzed.exe", flags);
     if(pe_information == NULL)
     {
         return 1;
